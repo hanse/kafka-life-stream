cd2d7798afc07b26cb7dce55bddd5602673844c1
Hans-Kristian Koren <hkri@koren.im>
2018-09-21 14:30:24 +0200

    Add arguments to console-consumer

--- console-consumer.js
+++ console-consumer.js
@@ -1,4 +1,10 @@
 const Kafka = require('node-rdkafka');
+const meow = require('meow');
+
+const cli = meow('console-consumer topic1,topic2 [--debug]');
+
+const topics = cli.input[0] || 'topic';
+const debug = !!cli.flags.debug;
 
 const consumer = new Kafka.KafkaConsumer(
   {
@@ -12,9 +18,11 @@ consumer.connect();
 
 consumer
   .on('ready', () => {
-    consumer.subscribe(['topic']);
+    consumer.subscribe(topics.split(',').map(topic => topic.trim()));
     consumer.consume();
   })
   .on('data', data => {
-    console.log(data.value.toString());
+    const value = data.value.toString();
+    const output = debug ? { ...data, value } : value;
+    console.log(output);
   });
