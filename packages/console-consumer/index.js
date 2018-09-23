6a91cce0229882214191c3b753e803a0123badf0
Hans-Kristian <hkri@koren.im>
2018-09-23 19:09:03 +0200

    Add dead-simple consumer abstraction

--- packages/console-consumer/index.js
+++ packages/console-consumer/index.js
@@ -1,28 +1,18 @@
-const Kafka = require('node-rdkafka');
 const meow = require('meow');
+const createConsumer = require('../create-consumer');
 
 const cli = meow('console-consumer topic1,topic2 [--debug]');
 
 const topics = cli.input[0] || 'topic';
 const debug = !!cli.flags.debug;
 
-const consumer = new Kafka.KafkaConsumer(
-  {
-    'group.id': 'kafka',
-    'metadata.broker.list': 'localhost:9092'
-  },
-  {}
-);
-
-consumer.connect();
-
-consumer
-  .on('ready', () => {
-    consumer.subscribe(topics.split(',').map(topic => topic.trim()));
-    consumer.consume();
-  })
-  .on('data', data => {
+const start = createConsumer(
+  topics.split(',').map(topic => topic.trim()),
+  data => {
     const value = data.value.toString();
     const output = debug ? { ...data, value } : value;
     console.log(output);
-  });
+  }
+);
+
+start();
---
+++ packages/create-consumer/index.js
@@ -0,0 +1,24 @@
+const Kafka = require('node-rdkafka');
+
+function createConsumer(topics, onMessage) {
+  const consumer = new Kafka.KafkaConsumer(
+    {
+      'group.id': 'kafka',
+      'metadata.broker.list': 'localhost:9092'
+    },
+    {}
+  );
+
+  return () => {
+    consumer
+      .on('ready', () => {
+        consumer.subscribe(topics);
+        consumer.consume();
+      })
+      .on('data', data => onMessage(data));
+
+    consumer.connect();
+  };
+}
+
+module.exports = createConsumer;
